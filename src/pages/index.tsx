02c9930a6dc0e642a8dad7a93fced315d681df81
Dawid Spisak <63721806+stairss@users.noreply.github.com>
2023-01-12 01:27:46 +0100

    feat: handle private routes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,16 +4,18 @@ import {
   createRoutesFromElements,
 } from "react-router-dom";
 import { Dashboard } from "../components/Dashboard";
+import PrivateRoutes from "../hooks/PrivateRoute";
 import { HomePage } from "./HomePage";
 import { LoginPage } from "./LoginPage";
 import { PersonalPage } from "./PersonalPage";
-
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
-      <Route path="/" element={<Dashboard />}>
-        <Route index element={<HomePage />} />
-        <Route path="/personal" element={<PersonalPage />} />
+      <Route element={<PrivateRoutes />}>
+        <Route path="/" element={<Dashboard />}>
+          <Route index element={<HomePage />} />
+          <Route path="/personal" element={<PersonalPage />} />
+        </Route>
       </Route>
       <Route path="/login" element={<LoginPage />} />
     </Route>
