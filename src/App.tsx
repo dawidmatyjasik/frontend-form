7b83d353af437611a1aba71c45202be4e94b8f47
Dawid Spisak <63721806+stairss@users.noreply.github.com>
2023-01-12 01:21:26 +0100

    feat: handle routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,35 @@
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import { Dashboard } from "./components/Dashboard";
-import { BrowserRouter as Router } from "react-router-dom";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import { LoginPage } from "./pages/LoginPage";
+import { HomePage } from "./pages/HomePage";
+import { PersonalPage } from "./pages/PersonalPage";
 
 const mdTheme = createTheme();
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<Dashboard />}>
+        <Route index element={<HomePage />} />
+        <Route path="/personal" element={<PersonalPage />} />
+      </Route>
+      <Route path="/login" element={<LoginPage />} />
+    </Route>
+  )
+);
 
 function App() {
   return (
     <ThemeProvider theme={mdTheme}>
       <CssBaseline />
-      <Router>
-        <Dashboard />
-      </Router>
+      <RouterProvider router={router} />
     </ThemeProvider>
   );
 }
