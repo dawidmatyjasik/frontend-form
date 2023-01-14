24cf8184ddfd3e3da8816f8db5cb813afc093978
Dawid Spisak <63721806+stairss@users.noreply.github.com>
2023-01-14 15:49:04 +0100

    feat: handle popover

---
+++ src/components/Popover/Popover.tsx
@@ -0,0 +1,54 @@
+import { Box, MenuItem, MenuList, Popover, Typography } from "@mui/material";
+
+interface IPopover {
+  anchorEl: any;
+  onClose: (props: any) => void;
+  open: boolean;
+}
+
+export const AccountPopover = (props: IPopover) => {
+  const { anchorEl, onClose, open, ...other } = props;
+
+  return (
+    <Popover
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        horizontal: "left",
+        vertical: "bottom",
+      }}
+      onClose={onClose}
+      open={open}
+      PaperProps={{
+        sx: { width: "300px" },
+      }}
+      {...other}
+    >
+      <Box
+        sx={{
+          py: 1.5,
+          px: 2,
+        }}
+      >
+        <Typography variant="overline">Account</Typography>
+        <Typography color="text.secondary" variant="body2">
+          John Doe
+        </Typography>
+      </Box>
+      <MenuList
+        disablePadding
+        sx={{
+          "& > *": {
+            "&:first-of-type": {
+              borderTopColor: "divider",
+              borderTopStyle: "solid",
+              borderTopWidth: "1px",
+            },
+            padding: "12px 16px",
+          },
+        }}
+      >
+        <MenuItem>Sign out</MenuItem>
+      </MenuList>
+    </Popover>
+  );
+};
