fe3775f689e47c08cfbf21e1d3e8731e0dd2e824
Dawid Spisak <63721806+stairss@users.noreply.github.com>
2023-02-04 16:52:49 +0100

    feat: connect redux query

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { pokemonApi } from "./api/personal";
 import authReducer from "./reducers/auth";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    [pokemonApi.reducerPath]: pokemonApi.reducer,
   },
   devTools: process.env.NODE_ENV !== "production",
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
-    }),
+    }).concat(pokemonApi.middleware),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
