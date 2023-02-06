700886c34e7a1a9d6209280a99e2c608501ab60e
Dawid Spisak <63721806+stairss@users.noreply.github.com>
2023-02-06 13:58:05 +0100

    feat: add toastify and filtering input

--- src/components/Table/Toolbar.tsx
+++ src/components/Table/Toolbar.tsx
@@ -2,23 +2,27 @@ import {
   GridToolbarContainer,
   GridToolbarDensitySelector,
   GridToolbarExport,
+  GridToolbarQuickFilter,
 } from "@mui/x-data-grid";
 import AddIcon from "@mui/icons-material/Add";
-import { Button } from "@mui/material";
+import { Box, Button } from "@mui/material";
 
 export const Toolbar = ({ setOpen }) => {
   return (
     <>
       <GridToolbarContainer>
-        <Button startIcon={<AddIcon />} onClick={() => setOpen(true)}>
-          Dodaj
-        </Button>
-        <GridToolbarDensitySelector
-          nonce={undefined}
-          onResize={undefined}
-          onResizeCapture={undefined}
-        />
-        <GridToolbarExport />
+        <Box style={{ flex: 1 }}>
+          <Button startIcon={<AddIcon />} onClick={() => setOpen(true)}>
+            Dodaj
+          </Button>
+          <GridToolbarDensitySelector
+            nonce={undefined}
+            onResize={undefined}
+            onResizeCapture={undefined}
+          />
+          <GridToolbarExport />
+        </Box>
+        <GridToolbarQuickFilter />
       </GridToolbarContainer>
     </>
   );
